c2ec031021ee11451587800ead310652ec8f5180
David McKenzie <david.mckenzie@jax.org>
2019-01-08 19:46:57 -0500

    ready for action

--- squares.py
+++ squares.py
@@ -2,17 +2,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-grid = [ [ '$5/sq', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J' ], 
-         [ '0', None, None, None, None, None, None, None, None, None ],
-         [ '1', None, 'Dave Mc', None, None, None, None, None, None, None ],
-         [ '2', None, 'Dave Mc', None, None, None, None, None, None, None ],
-         [ '3', None, None, None, None, None, None, None, None, None ],
-         [ '4', None, None, None, None, None, None, None, None, None ],
-         [ '5', None, None, None, None, None, None, None, None, None ],
-         [ '6', None, None, None, None, None, None, None, None, None ],
-         [ '7', None, None, None, None, None, None, None, 'Dave Mc', None ],
-         [ '8', None, None, None, None, None, None, None, 'Dave Mc', None ],
-         [ '9', None, None, None, None, None, None, None, None, None ]]
+
+grid = [ [ '$5/sq', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J' ],
+         [ '0', '', '', '', '', '', '', '', '', '', ''],
+         [ '1', '', 'Dave Mc', '', '', '', '', '', '', '', ''],
+         [ '2', '', 'Dave Mc', '', '', '', '', '', '', '', ''],
+         [ '3', '', '', '', '', '', '', '', '', '', ''],
+         [ '4', '', '', '', '', '', '', '', '', '', ''],
+         [ '5', '', '', '', '', '', '', '', '', '', ''],
+         [ '6', '', '', '', '', '', '', '', '', '', ''],
+         [ '7', '', '', '', '', '', '', '', '', 'Dave Mc', ''],
+         [ '8', '', '', '', '', '', '', '', '', 'Dave Mc', ''],
+         [ '9', '', '', '', '', '', '', '', '', '', '']]
 
 @app.route("/")
 def index():
